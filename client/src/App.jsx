8c33e8e10d53c760f81e9b01ec1379558a4e476a
pankaj0114 <navepala@gmail.com>
2024-06-05 19:58:49 +0530

    add more features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,44 @@
 import React , {lazy} from 'react';
 import {BrowserRouter , Routes , Route} from 'react-router-dom';
-import Groups from './pages/Groups';
+
+import ProtectRoute from './components/auth/ProtectRoute';
 
 const Home = lazy (()=> import ("./pages/Home"));
 const Login = lazy (()=> import ("./pages/Login"));
 
 const Chat = lazy(()=> import ("./pages/chat"));
+const Groups = lazy(()=> import ("./pages/Groups"));
+
+
+let user = true;
+
+
+
 
 const App = () => {
   return (
    <BrowserRouter>
   <Routes>
-  <Route path="/" element ={<Home />} />
-   <Route path="/login" element ={<Login />} />
+   <Route element={<ProtectRoute user={user}/>}>
+   <Route path="/" element ={<Home/>} />
+   
    <Route path="/chat/:chatId" element ={<Chat />} />
    <Route path="/Groups" element ={<Groups/>} />
+   </Route>
+
+   <Route path="/login" element ={
+      <ProtectRoute user={!user} redirect="/">
+        <Login />
+      </ProtectRoute>
+    } />
+
+    <Route path="*" element={<Home/>}/>
   </Routes>
+  
    
    </BrowserRouter>
   );
+
 };
 
 export default App;
