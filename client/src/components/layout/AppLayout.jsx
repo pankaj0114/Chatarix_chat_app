36b7ed4951a3c1469c0149830b3326a4f9a705ed
pankaj0114 <navepala@gmail.com>
2024-06-09 19:55:30 +0530

    Done of the day

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react' 
 import Header from './Header';
 import Title from "../shared/Title";
+import {Grid} from "@mui/material";
 
 const AppLayout = () => WrappedComponent=>{
   return(props)=> {
@@ -8,12 +9,45 @@ const AppLayout = () => WrappedComponent=>{
     <>
     <Title />
     <Header/>
+
+   <Grid container height ={"calc(100vh - 4rem)"}>
+
+
+    <Grid 
+    item 
+    sm={4} 
+    md={3} 
+    sx={
+      {
+        display : {xs: "none", sm:"block"},
+      }
+    }  
+    
+    height={"100%"} >
+    First
+    </Grid>
+
+    <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"} >
     <WrappedComponent {...props}/>
-    <div>Footer</div>
+    </Grid>
+    <Grid item  md={4} lg={3} height={"100%"}
+    sx={
+      {
+        display : {xs: "none", md:"block"},
+        padding: "2rem",
+        bgcolor:"#039be5"
+      }
+    } 
+    >
+      Thrid
+    </Grid>
+   </Grid> 
+    
+   
     </>
     );
 };
-   
+  
     
   
 };
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,9 +1,17 @@
+import { AppBar, Box } from '@mui/material';
+
 import React from 'react'
 
+
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <>
+  <Box sx={{flexGrow:1}} height={"4rem"}>
+    <AppBar position="static"
+    sx={{
+      bgcolor:"#81d4fa"
+    }} />
+  </Box>
+  </>
+};
 
 export default Header
